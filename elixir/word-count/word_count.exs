8cb92270b2c97f91ed400b79e5fc157ce6466b16
julien michot <ju.michot@gmail.com>
2015-10-22 17:50:01 +0200

    Make word count pass

--- elixir/word-count/word_count.exs
+++ elixir/word-count/word_count.exs
@@ -6,9 +6,17 @@ defmodule Words do
   """
   @spec count(String.t) :: map()
   def count(sentence) do
-    words = String.split(sentence)
-    result = Enum.reduce(words, %{}, fn(item, acc) ->
-      put_in(acc, [item], 1)
+    sentence
+    |> String.downcase
+    |> String.replace(~r{[#,!@&\^$%:_]}, " ")
+    |> String.split
+    |> Enum.reduce(%{}, fn(item, acc) ->
+      case acc do
+        %{^item => a_val} ->
+          Dict.put(acc, item, a_val + 1)
+        _ ->
+          put_in(acc, [item], 1)
+      end
     end)
   end
 end
