93c94d7863d5af5906c849cb22d0211e0ccf7f18
julien michot <ju.michot@gmail.com>
2015-10-24 17:15:13 +0200

    Starting reduce

--- elixir/list-ops/list_ops.exs
+++ elixir/list-ops/list_ops.exs
@@ -9,56 +9,100 @@ defmodule ListOps do
   @spec count(list) :: non_neg_integer
 
   def count(l) do
-    _length(l)
+    # _length(l)
+    _reduce(l,0, fn(_,acc) -> 1 + acc  end)
   end
 
   defp _length([]), do: 0
-  defp _length([head|tail]) do
+  defp _length([_|tail]) do
     1 + _length(tail)
   end
 
   @spec reverse(list) :: list
   def reverse(l) do
-    _reverse(l)
+    # _reverse(l)
+    _reduce(l, [], fn(l, acc) -> [l|acc] end)
   end
 
-  defp _reverse(head), do: _reverse(head,[])
-  defp _reverse([], result), do: result
-  defp _reverse([head|tail], result) do
-    _reverse(tail, [head] ++ result)
-  end
+  # defp _reverse(head), do: _reverse(head,[])
+  # defp _reverse([], result), do: result
+  # defp _reverse([head|tail], result) do
+  #   _reverse(tail, [head|result])
+  # end
 
 
   @spec map(list, (any -> any)) :: list
   def map(l, f) do
-    _map(l,f)
+    # _map(l,f)
+    l
+    |> _reduce([], &([f.(&1) | &2]))
+    |> reverse
   end
 
-  defp _map([], _func), do: []
-  defp _map([head|tail], func) do
-    [func.(head) | _map(tail, func)]
-  end
+  # defp _map([], _func), do: []
+  # defp _map([head|tail], func) do
+  #   [func.(head) | _map(tail, func)]
+  # end
 
   @spec filter(list, (any -> as_boolean(term))) :: list
   def filter(l, f) do
-    _filter(l,f)
+    # _filter(l,f)
+    _reduce(l,[], fn(h,acc) ->
+      cond do
+        f.(h) -> [h|acc]
+        true -> acc
+      end
+    end) |> reverse
   end
 
-  defp _filter([], func), do: []
-
-  # @type acc :: any
-  # @spec reduce(list, acc, ((any, acc) -> acc)) :: acc
-  # def reduce(l, acc, f) do
-
+  # defp _filter([], func), do: []
+  # defp _filter([head|tail], func) do
+  #   cond do
+  #     func.(head) ->
+  #       [head | _filter(tail, func)]
+  #     true ->
+  #       _filter(tail, func)
+  #   end
   # end
 
-  # @spec append(list, list) :: list
-  # def append(a, b) do
+  @type acc :: any
+  @spec reduce(list, acc, ((any, acc) -> acc)) :: acc
+  def reduce(l, acc, f) do
+    _reduce(l, acc, f)
+  end
 
-  # end
+  defp _reduce([], acc, _), do: acc
+  defp _reduce([head|tail], acc, f) do
+    _reduce(tail, f.(head,acc), f)
+  end
+
+  @spec append(list, list) :: list
+  def append(a, b) do
+    _append(a,b)
+  end
+
+
+  defp _append([],[]), do: []
+  defp _append([], b), do: b
+  defp _append(a, []), do: a
+  defp _append(a,b) do
+  end
+
+
 
   # @spec concat([[any]]) :: [any]
   # def concat(ll) do
 
   # end
 end
+  # def flatten([]), do: []
+  # def flatten([head | tail]) do
+  #   [flatten(head) ++ flatten(tail)]
+  # end
+  # def flatten(head), do: [head]
+
+  # def flatten([]), do: []
+  # def flatten([head]) when not is_list(head), do: [head]
+  # def flatten([head|tail]) do
+  #   flatten(head) ++ flatten(tail)
+  # end
--- elixir/list-ops/list_ops_test.exs
+++ elixir/list-ops/list_ops_test.exs
@@ -62,43 +62,47 @@ defmodule ListOpsTest do
     assert L.filter([], &odd?/1) == []
   end
 
-  # test "filter of normal list" do
-  #   assert L.filter([1,2,3,4], &odd?/1) == [1,3]
-  # end
+  test "filter of normal list" do
+    assert L.filter([1,2,3,4], &odd?/1) == [1,3]
+  end
 
-  # test "filter of huge list" do
-  #   assert L.filter(Enum.to_list(1..1_000_000), &odd?/1) ==
-  #     Enum.map(1..500_000, &(&1*2-1))
-  # end
+  test "filter of ndormal list" do
+    assert L.filter([2,2,3,4], &odd?/1) == [3]
+  end
 
-  # test "reduce of empty list" do
-  #   assert L.reduce([], 0, &(&1+&2)) == 0
-  # end
+  test "filter of huge list" do
+    assert L.filter(Enum.to_list(1..1_000_000), &odd?/1) ==
+      Enum.map(1..500_000, &(&1*2-1))
+  end
 
-  # test "reduce of normal list" do
-  #   assert L.reduce([1,2,3,4], -3, &(&1+&2)) == 7
-  # end
+  test "reduce of empty list" do
+    assert L.reduce([], 0, &(&1+&2)) == 0
+  end
 
-  # test "reduce of huge list" do
-  #   assert L.reduce(Enum.to_list(1..1_000_000), 0, &(&1+&2)) ==
-  #     Enum.reduce(1..1_000_000, 0, &(&1+&2))
-  # end
+  test "reduce of normal list" do
+    assert L.reduce([1,2,3,4], -3, &(&1+&2)) == 7
+  end
 
-  # test "reduce with non-commutative function" do
-  #   assert L.reduce([1,2,3,4], 10, fn x, acc -> acc - x end) == 0
-  # end
+  test "reduce of huge list" do
+    assert L.reduce(Enum.to_list(1..1_000_000), 0, &(&1+&2)) ==
+      Enum.reduce(1..1_000_000, 0, &(&1+&2))
+  end
 
-  # test "append of empty lists" do
-  #   assert L.append([], []) == []
-  # end
+  test "reduce with non-commutative function" do
+    assert L.reduce([1,2,3,4], 10, fn x, acc -> acc - x end) == 0
+  end
 
-  # test "append of empty and non-empty list" do
-  #   assert L.append([], [1,2,3,4]) == [1,2,3,4]
-  # end
+  test "append of empty lists" do
+    assert L.append([], []) == []
+  end
 
-  # test "append of non-empty and empty list" do
-  #   assert L.append([1,2,3,4], []) == [1,2,3,4]
-  # end
+  test "append of empty and non-empty list" do
+    assert L.append([], [1,2,3,4]) == [1,2,3,4]
+  end
+
+  test "append of non-empty and empty list" do
+    assert L.append([1,2,3,4], []) == [1,2,3,4]
+  end
 
   # test "append of non-empty lists" do
   #   assert L.append([1,2,3], [4,5]) == [1,2,3,4,5]
@@ -108,6 +112,9 @@ defmodule ListOpsTest do
   #   assert L.append(Enum.to_list(1..1_000_000), Enum.to_list(1_000_001..2_000_000)) ==
   #     Enum.to_list(1..2_000_000)
   # end
+
+
+
 
   # test "concat of empty list of lists" do
   #   assert L.concat([]) == []
@@ -127,3 +134,14 @@ defmodule ListOpsTest do
   #     Enum.to_list(1..1_000_000)
   # end
 end
+  # test "flatten of  lists" do
+  #   assert L.flatten([]) == []
+  #   assert L.flatten([1]) == [1]
+  # end
+  # test "flatten of  lol lists" do
+  #   assert L.flatten([[1, 2],2]) == [1,2,2]
+  # end
+
+  # test "flatten of non-empty lists" do
+  #   assert L.flatten([1,2, [2, [4,5,6]]]) == [1,2,3,4,5,6]
+  # end
